b6ba2029ae1d30f89359fff1c92a140c109c8112
Vo Pham Khanh Truong <voxtruong93@gmail.com>
2024-08-07 15:54:08 +0700

    update

--- .history/src/main_20240807154912.jsx
+++ .history/src/main_20240807154912.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
@@ -5,10 +6,10 @@ import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/s
 import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <React.StrictMode>
     <CssVarsProvider theme={theme}>
       <CssBaseline />
       <App />
     </CssVarsProvider>
-  </>
+  </React.StrictMode>
 )
