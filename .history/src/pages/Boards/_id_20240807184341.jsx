1609dc3e4b92686c9a1110f5275725a7d85b9561
Vo Pham Khanh Truong <voxtruong93@gmail.com>
2024-08-07 19:08:05 +0700

    project structure

---
+++ .history/src/pages/Boards/_id_20240807184341.jsx
@@ -0,0 +1,12 @@
+//Board Details
+import React from 'react'
+
+function Board() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Board
---
+++ .history/src/pages/Boards/_id_20240807184357.jsx
@@ -0,0 +1,12 @@
+//Board Details
+import React from 'react'
+
+function Board() {
+  return (
+    <div>
+      _id
+    </div>
+  )
+}
+
+export default Board
